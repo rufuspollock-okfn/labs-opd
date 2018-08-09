a6fd21d40f292c0367937e0f5fb91864eaf47a44
Brook Elgie <specialbrew@gmail.com>
2018-08-09 13:47:11 +0100

    Add features to admin to allow product editing

--- browser/admin.py
+++ browser/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from browser.models import Brand, Gtin
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('BSIN', 'BRAND_NM')
+
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class GtinAdmin(admin.ModelAdmin):
+    list_display = ('GTIN_CD', 'GTIN_NM')
+    search_fields = ['GTIN_CD', 'GTIN_NM']
+
+
+admin.site.register(Gtin, GtinAdmin)
